1fe7a99223425b18072464f919e5647fa46a013b
Édouard Touraile <edouard.touraille@gmail.com>
2015-05-04 14:14:04 +0200

    Change manifest

---
+++ www/notification.js
@@ -0,0 +1,27 @@
+var notification = {
+    listener : function(event, data){}, // we define a signature for the methode
+    
+    listenerCallback : function(event, data){ // te callback call that signature, probably impossible to redefine a function is it already defined see redefinition method in js
+        amqp.listener(event, data);
+    },
+    
+    register : function(listener){
+        
+        function successCb(){
+            notification.listener = listener;
+        }
+        function errorCb(){
+            console.log('Error while registration');
+        }
+        
+        cordova.exec(
+            successCb,
+            errorCb,
+            'AMQP',
+            'register',
+            [{'notificationListener' : 'notification.listenerCallback'}]
+        );
+        
+    },
+};
+
