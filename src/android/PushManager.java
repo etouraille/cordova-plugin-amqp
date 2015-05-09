a070017943fe2518f7df6bf268635094cfc4f24e
Édouard Touraile <edouard.touraille@gmail.com>
2015-05-09 11:47:57 +0200

    CO

--- src/android/PushManager.java
+++ src/android/PushManager.java
@@ -2,17 +2,19 @@ package org.amqp.notification;
 
 //org.amqp
 import org.amqp.notification.PushService;
+import org.amqp.notification.Push;
+
 //android 
 import android.app.Activity;
 import android.content.Intent;
-
+import android.content.IntentSender;
 import android.util.Log;
 
 class PushManager  {
 
-    public PushManager( Activity activity ) {
+    public PushManager( Activity activity , Push push) {
         
-        Intent intent = new Intent( this, PushService.class);
+        Intent intent = new Intent( activity, PushService.class);
         Log.e("BEFORE START SERVICE","1");
         //Intent intent = new Intent("org.amqp.notification.PushService");
         activity.startService(intent);
